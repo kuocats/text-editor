494aa96a50d821271be4b9bc4149e449f80c33ab
Vivian Kuo <vivian.kuo4@outlook.com>
2023-09-08 14:01:19 -1000

    removed babel/plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -60,7 +60,7 @@ module.exports = () => {
 						options: {
 							presets: ['@babel/preset-env'],
 							plugins: [
-								'@babel/plugin-proposal-object-rest-spread',
+								//'@babel/plugin-proposal-object-rest-spread',
 								'@babel/transform-runtime',
 							],
 						},
